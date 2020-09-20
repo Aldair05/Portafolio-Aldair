7f9b96ef50ebbf9fb906ddee022d62c46affa7f6
aldair05 <aldair.uaem@gmail.com>
2020-09-20 00:31:13 -0500

    Se actualizo menu y proyectos

--- js/script.js
+++ js/script.js
@@ -4,28 +4,44 @@ const navegador = document.getElementById('navega');
 
 
 
+// AddeventListener();
 
+// //AddeventListener
+// function AddeventListener(){
 
-AddeventListener();
-
-//AddeventListener
-function AddeventListener(){
-
-    //Mostrar el menu
-mostrarMenu.addEventListener('click',MostrarMenu);
+//     //Mostrar el menu
+// mostrarMenu.addEventListener('click',MostrarMenu);
         
-}
+// }
 
 
-//FUNCIONES
-function MostrarMenu(e){
-    e.preventDefault();
-    navegador.classList.toggle('mostrar');
-    // mostrarMenu.style.color = 'white';
-}
+// //FUNCIONES
+// function MostrarMenu(e){
+//     e.preventDefault();
+//     navegador.classList.toggle('mostrar');
+//     // mostrarMenu.style.color = 'white';
+// }
+
+
+const hamburger = document.querySelector('.hamburger-btn');
+var hamburgerMenuOpen = false;
 
 
 
+hamburger.addEventListener("click", () => {
+    
+    if(hamburgerMenuOpen){
+        hamburgerMenuOpen = !hamburgerMenuOpen;
+        hamburger.classList.remove("open");
+        navegador.classList.toggle('mostrar');
+        
+    }else{
+        hamburgerMenuOpen = !hamburgerMenuOpen;
+        hamburger.classList.add("open");
+        navegador.classList.toggle('mostrar');
+    }
+        // navegador.classList.toggle('mostrar');
+});
 
 
 
